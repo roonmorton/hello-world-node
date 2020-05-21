d011b02ce0f5b5741d4f355ffd6b5fbe56f9329f
Ron <roonmorton@gmail.com>
2020-05-20 23:21:08 -0600

    commit inicial

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const os = require("os")
+const ip = require('ip')
+const PORT = 8080
+const HOST = '0.0.0.0'
+const app = express()
+app.get('/', (req, res) => {
+    res.jsonp({
+        message: "Hello World!",
+        version: "1.0.0",
+        Hostname: os.hostname(),
+        "IP Address": ip.address()
+    })
+})
+app.listen(PORT, HOST)
+console.log(`Running on port ${PORT}`)
